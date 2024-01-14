498af61ab97cdb1eca5ac59ea82e25ba5578f5a2
Benjamin Wachira <wachirabenjamin18@gmail.com>
2024-01-14 07:27:38 -0800

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ import models
 from models.city import City
 import shlex
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
