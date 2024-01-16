640d4ee9c57118e858a8c62f19b48b34bdccb9cf
mistidevs-alx <mistidevs@gmail.com>
2024-01-16 03:09:58 -0800

    Revert to Former

--- models/base_model.py
+++ models/base_model.py
@@ -15,25 +15,9 @@ class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
-    storage = os.environ.get("HBNB_TYPE_STORAGE")
-    if storage == "db":
-        id = Column(String(60), unique=True, nullable=False, primary_key=True)
-        created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-        updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    else:
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
