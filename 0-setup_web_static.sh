6afba84ab658c0524c3c602fcfe211836646f1fe
mistidevs-alx <mistidevs@gmail.com>
2024-02-01 10:20:18 -0800

    Alias

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,28 @@ mkdir -p /data/web_static/releases/test/
 echo "Hello, Nginx here" > /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo nginx -t -c "/data/web_static/current/ /hbnb_static"
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 sudo service nginx restart
