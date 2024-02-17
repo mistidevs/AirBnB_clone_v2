cc713f97f701d6d7b3dc66d51bf43893294eefbc
mistidevs-alx <mistidevs@gmail.com>
2024-02-17 10:26:31 -0800

    Hello Flask!!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Using flask to start a simple web server
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
