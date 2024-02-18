c9e3beda92bb0518c5ece850021d4c20b41f3c01
mistidevs-alx <mistidevs@gmail.com>
2024-02-18 06:52:18 -0800

    Jinja

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,10 +10,10 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def states():
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+List of states
+"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>')
+def cities(id):
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    for state in states:
+        if state.id == id:
+            cities = sorted(state.cities, key=lambda city: city.name)
+            return render_template('9-states.html', state=state, cities=cities)
+    return render_template('9-states.html')
+
+
+@app.teardown_appcontext
+def closing(exception=None):
+    storage.close()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
